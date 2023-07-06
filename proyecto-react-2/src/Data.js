4c6792f3392f4e7727704eeef124a210ae965388
Hotaru9 <hotarupd969@gmail.com>
2023-07-05 18:23:04 -0600

    data

---
+++ proyecto-react-2/src/Data.js
@@ -0,0 +1,80 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "The art of Amano",
+      price: 500,
+      image: require('./Imagen/amano.jpg'),
+      category: "book",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Boogiepop",
+      price: 350,
+      image: require('./Imagen/boogie.jpg'),
+      category: "book",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Children of sea",
+      price: 300,
+      image: require('./Imagen/children.jpg'),
+      category: "manga",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Emanon 1",
+      price: 250,
+      image: require('./Imagen/emanon.jpg'),
+      category: "book",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Houseki no Kuni",
+      price: 200,
+      image: require('./Imagen/houseki.jpg'),
+      category: "manga",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Mushishi",
+      price: 200,
+      image: require('./Imagen/mushi.jpg'),
+      category: "manga",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Owarimonogatari",
+      price: 350,
+      image: require('./Imagen/owari.jpg'),
+      category: "book",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Solanin",
+      price: 350,
+      image: require('./Imagen/solanin.jpg'),
+      category: "manga",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Yokohama Kaidosha",
+      price: 200,
+      image: require('./Imagen/yokohama.jpg'),
+      category: "manga",
+      cantidad: 1
+    },
+
+  ],
+};
+
+export default data;
